6f57428a0cfdf7231f8613e0ff310ab3b165413b
“jms” <jstapleton536@gmail.com>
2022-09-17 10:46:51 -0400

    insert statements

--- sql/seeds.sql
+++ sql/seeds.sql
@@ -1,1 +1,15 @@
 import { v4 as uuid } from 'uuid';
+
+INSERT INTO department() 
+VALUES (uuid(), name),
+        (uuid(), name),
+        (uuid(), name),
+        (uuid(), name);
+
+INSERT INTO role ()
+VALUES (uuid(), title, salary, department_id),
+        (uuid(), title, salary, department_id);
+
+INSERT INTO employee()
+VALUES (uuid(), first_name, last_name, role_id, manager_id),
+        (uuid(), first_name, last_name, role_id, manager_id);
