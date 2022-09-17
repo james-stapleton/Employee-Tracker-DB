4665a07590d7da0806e905057e8a68dc724364a7
“jms” <jstapleton536@gmail.com>
2022-09-17 09:47:32 -0400

    File structure and sql starter code

---
+++ sql/schemas.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employee_tracker;
+CREATE DATABASE employee_tracker;
+
+use employee_tracker;
+
+CREATE TABLE department (
+    id int PRIMARY KEY NOT NULL,
+    name VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE role (
+    id INT PRIMARY KEY NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+        REFERENCES(department(id))
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT FOREIGN KEY (manager_id) REFERENCES employee(id),
+    FOREIGN KEY (role_id)
+        REFERENCES(role(id))
+);
