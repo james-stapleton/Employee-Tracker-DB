1aa561bf382acc8ef75fa29c421217283416378d
James Stapleton <jstapleton536@gmail.com>
2022-09-20 17:28:37 -0400

    setup connection to database

---
+++ db/connection.js
@@ -0,0 +1,24 @@
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
+const fs = require("fs");
+
+dotenv.config()
+
+const connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST || "localhost",
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_NAME,
+    port: process.env.MYSQL_PORT
+})
+
+//! const seed = fs.readFileSync("../sql/seeds.sql", {encoding: "UTF-8"})
+
+connection.query(
+    'SELECT * FROM `department`',
+    function(err, results) {
+      console.log(results);
+    }
+  );
+
+  module.exports = connection;
--- sql/seeds.sql
+++ sql/seeds.sql
@@ -1,16 +1,15 @@
-import { v4 as uuid } from 'uuid';
 
-INSERT INTO department() 
-VALUES (uuid(), Engineering),
-        (uuid(), Finance),
-        (uuid(), Legal),
-        (uuid(), Sales);
+INSERT INTO department(id, name) 
+VALUES (1, "Engineering"),
+        (2, "Finance"),
+        (3, "Legal"),
+        (4, "Sales");
 
-INSERT INTO role ()
-VALUES (uuid(), "Lead Engineer", 150000.00, department_id),
-        (uuid(), "Engineer", 100000.00, department_id);
+-- INSERT INTO role (title, salary, department_id)
+-- VALUES ("Lead Engineer", 150000.00, 1),
+--         ("Engineer", 100000.00, department_id);
 
-INSERT INTO employee()
-VALUES (uuid(), "James", "Stapleton", role_id, manager_id),
-        (uuid(), "Joe", "Smith", role_id, manager_id),
-        (uuid(), "Jane", "Smith", role_id, manager_id);
+-- INSERT INTO employee(first_name, last_name, role_id, manager_id)
+-- VALUES ("James", "Stapleton", role_id, manager_id),
+--         ("Joe", "Smith", role_id, manager_id),
+--         ("Jane", "Smith", role_id, manager_id);
